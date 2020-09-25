d60a0a0f5ed58f34ac95a4fa8de69bc9b3592a32
thaynamarinss <thayna.mgas@hotmail.com>
2020-09-25 17:23:30 -0300

    Desafio 1

---
+++ JS_assincrono/desafios.js
@@ -0,0 +1,23 @@
+//Desafio 1
+/*
+var idade = parseInt(prompt('Digite sua idade: '), 10);
+function checaIdade(idade) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if(idade >= 18){
+                resolve()
+            } else {
+                reject()
+            }
+        }, 2000)
+    })
+
+}
+checaIdade(idade)
+    .then(function () {
+        console.log("Maior que 18");
+    }).catch(function () {
+        console.log("Menor que 18");
+    });
+
+*/
