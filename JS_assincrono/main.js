7884b7fcfb2bcbc032680ef17de4b9ddaee9f261
thaynamarinss <thayna.mgas@hotmail.com>
2020-09-25 17:04:57 -0300

    Promises

--- JS_assincrono/main.js
+++ JS_assincrono/main.js
@@ -11,3 +11,33 @@ xhr.onreadystatechange = function(){
     }
 }
 */
+
+//Promises
+
+var minhaPromise = function(){
+    return new Promise(function(resolve, reject){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users/diego3g');
+    xhr.send(null);
+
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4){
+            if (xhr.status === 200){
+                resolve(JSON.parse(xhr.respondeText));
+            }else {
+                reject('Erro na requisição');
+            }
+
+
+            }
+        }
+    });
+}
+
+minhaPromise()
+    .then(function(responde){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
